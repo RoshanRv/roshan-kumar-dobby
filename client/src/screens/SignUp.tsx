cd01632979f011a0f0c0a0dfce8e46e4bc817bf0
RoshanRv <nkroshankumar@gmail.com>
2023-05-31 21:09:03 +0530

    signin , signup, useuser store. page created

---
+++ client/src/screens/SignUp.tsx
@@ -0,0 +1,81 @@
+import axios from "axios"
+import React, { useState } from "react"
+import { RiH1 } from "react-icons/ri"
+import { Link, useNavigate } from "react-router-dom"
+
+type Props = {}
+
+const SignUp = (props: Props) => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const navigate = useNavigate()
+    const [err, setErr] = useState("")
+
+    const handleSignUp = async () => {
+        try {
+            const user = await axios.post(
+                `${import.meta.env.VITE_SERVER_ENDPOINT}/api/user`,
+                { username, password, email },
+                {
+                    withCredentials: true,
+                }
+            )
+            navigate("/signin")
+        } catch (e: any) {
+            console.log(e)
+            setErr(e.response.data)
+        }
+    }
+
+    return (
+        <main className="pt-24 px-10 h-screen w-full  bg-gradient-to-br from-gray-700 to-gray-900 flex justify-center items-center">
+            {/* Form */}
+            <div className="bg-white shadow-xl p-5 flex flex-col gap-y-4  rounded-md lg:w-5/12 ">
+                <h1 className="font-semibold text-3xl">SignUp</h1>
+                {/* name */}
+                <input
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    type="text"
+                    className="p-3 border-2 border-gray-400 rounded-md  placeholder:text-gray-500 outline-0 w-full"
+                    placeholder="Username"
+                />
+                {/* email */}
+                <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="text"
+                    className="p-3 border-2 border-gray-400 rounded-md  placeholder:text-gray-500 outline-0 w-full"
+                    placeholder="Email"
+                />
+                {/* password */}
+                <input
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    className="p-3 border-2 border-gray-400 rounded-md  placeholder:text-gray-500 outline-0 w-full"
+                    placeholder="Password"
+                />
+                {err && (
+                    <h1 className="bg-red-700 text-white p-2 font-semibold text-center ">
+                        {err}
+                    </h1>
+                )}
+                <button
+                    onClick={handleSignUp}
+                    className="w-full rounded-lg p-2 py-3 active:scale-95 transition-all font-semibold text-white bg-gradient-to-br from-gray-700 to-gray-900 outline-0"
+                >
+                    SignUp
+                </button>
+                <Link to={"/signin"}>
+                    <p className="text-center capitalize text-sm cursor-pointer text-gray-700 hover:underline underline-offset-4">
+                        Already Have Account? SignIn
+                    </p>
+                </Link>
+            </div>
+        </main>
+    )
+}
+
+export default SignUp
